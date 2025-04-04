f1f292da8fc455fe5150409aead6f1d84cd5d7ea
yoorkim <amy0848@naver.com>
2025-04-04 15:10:24 +0900

    feat(#7) : Scrap CRUD

---
+++ src/main/java/org/pinggu/portforu/domain/scrap/entity/Scrap.java
@@ -0,0 +1,32 @@
+package org.pinggu.portforu.domain.scrap.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.pinggu.portforu.common.domain.BaseEntity;
+import org.pinggu.portforu.domain.jobposting.entity.JobPosting;
+import org.pinggu.portforu.domain.member.entity.Member;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "scraps")
+public class Scrap extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "jobposting_id", nullable = false)
+    private JobPosting jobPosting;
+
+    public Scrap(Member member, JobPosting jobPosting) {
+        this.member = member;
+        this.jobPosting = jobPosting;
+    }
+
+}
