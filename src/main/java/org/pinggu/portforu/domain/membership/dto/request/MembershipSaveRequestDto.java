e339c154ceae209fe33f7e582aef0024a8d7d438
ChungWon <wjdcjddnjs1234@gmail.com>
2025-04-07 17:02:57 +0900

    ref(#4) : add NoArgsConstructor

--- src/main/java/org/pinggu/portforu/domain/membership/dto/request/MembershipSaveRequestDto.java
+++ src/main/java/org/pinggu/portforu/domain/membership/dto/request/MembershipSaveRequestDto.java
@@ -3,9 +3,11 @@ package org.pinggu.portforu.domain.membership.dto.request;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MembershipSaveRequestDto {
     @NotNull(message = "멤버쉽 이름은 필수입니다.")
     private String name;
--- src/main/java/org/pinggu/portforu/domain/membership/dto/request/MembershipUpdateRequestDto.java
+++ src/main/java/org/pinggu/portforu/domain/membership/dto/request/MembershipUpdateRequestDto.java
@@ -2,9 +2,11 @@ package org.pinggu.portforu.domain.membership.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MembershipUpdateRequestDto {
     private String name;
     private Integer price;
