b26398027a044c34f7d6b58c1d7578727a54e6bd
ChungWon <wjdcjddnjs1234@gmail.com>
2025-04-07 16:59:50 +0900

    ref(#4) : add NotNull

--- src/main/java/org/pinggu/portforu/domain/membership/dto/request/MembershipSaveRequestDto.java
+++ src/main/java/org/pinggu/portforu/domain/membership/dto/request/MembershipSaveRequestDto.java
@@ -1,15 +1,18 @@
 package org.pinggu.portforu.domain.membership.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class MembershipSaveRequestDto {
+    @NotNull(message = "멤버쉽 이름은 필수입니다.")
     private String name;
+    @NotNull(message = "가격은 필수입니다.")
     private Integer price;
+    @NotNull(message = "수량은 필수입니다.")
     private Integer quantity;
+    @NotNull(message = "사용년도는 필수입니다.")
     private Integer year;
 }
