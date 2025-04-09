7f138957c3a77ecde0e7077cb424228601947cf0
ChungWon <wjdcjddnjs1234@gmail.com>
2025-04-09 10:40:50 +0900

    ref : Removing the Membership Controller

--- src/main/java/org/pinggu/portforu/domain/membership/controller/MembershipAdminController.java
+++ src/main/java/org/pinggu/portforu/domain/membership/controller/MembershipAdminController.java
@@ -3,23 +3,19 @@ package org.pinggu.portforu.domain.membership.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.pinggu.portforu.common.annotation.Admin;
-import org.pinggu.portforu.common.domain.PageInfo;
-import org.pinggu.portforu.common.domain.Pagecond;
 import org.pinggu.portforu.common.dto.ApiResponse;
 import org.pinggu.portforu.domain.membership.dto.request.MembershipSaveRequestDto;
 import org.pinggu.portforu.domain.membership.dto.request.MembershipUpdateRequestDto;
 import org.pinggu.portforu.domain.membership.dto.response.MembershipResponseDto;
 import org.pinggu.portforu.domain.membership.service.MembershipService;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/memberships")
-public class MembershipController {
+public class MembershipAdminController {
+
     private final MembershipService membershipService;
 
     @Admin
@@ -28,28 +24,6 @@ public class MembershipController {
             @Valid @RequestBody MembershipSaveRequestDto request
     ) {
         return ResponseEntity.ok().body(ApiResponse.of(membershipService.saveMembership(request)));
-    }
-
-    @GetMapping
-    public ResponseEntity<ApiResponse<List<MembershipResponseDto>>> findAllMemberships(
-            @ModelAttribute Pagecond pagecond
-    ) {
-        Page<MembershipResponseDto> responses = membershipService.findAllMemberships(pagecond);
-        PageInfo pageInfo = PageInfo.builder()
-                .pageNum(pagecond.getPageNum())
-                .pageSize(pagecond.getPageSize())
-                .totalElement(responses.getTotalElements())
-                .totalPage(responses.getTotalPages())
-                .build();
-
-        return ResponseEntity.ok().body(ApiResponse.of(responses.getContent(), pageInfo));
-    }
-
-    @GetMapping("/{membershipId}")
-    public ResponseEntity<ApiResponse<MembershipResponseDto>> findMembershipById(
-            @PathVariable("membershipId") Long membershipId
-    ) {
-        return ResponseEntity.ok().body(ApiResponse.of(membershipService.findMembershipById(membershipId)));
     }
 
     @Admin
@@ -70,5 +44,4 @@ public class MembershipController {
 
         return ResponseEntity.ok().body(ApiResponse.of(deletedMembershipId));
     }
-
 }
