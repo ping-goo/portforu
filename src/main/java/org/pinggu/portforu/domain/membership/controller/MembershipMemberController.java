8ac04c196e6826d95bd321ffe792aafd78611fb3
ChungWon <wjdcjddnjs1234@gmail.com>
2025-04-09 10:40:59 +0900

    ref : Removing the Membership Controller

---
+++ src/main/java/org/pinggu/portforu/domain/membership/controller/MembershipMemberController.java
@@ -0,0 +1,42 @@
+package org.pinggu.portforu.domain.membership.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.pinggu.portforu.common.domain.PageInfo;
+import org.pinggu.portforu.common.domain.Pagecond;
+import org.pinggu.portforu.common.dto.ApiResponse;
+import org.pinggu.portforu.domain.membership.dto.response.MembershipResponseDto;
+import org.pinggu.portforu.domain.membership.service.MembershipService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/memberships")
+public class MembershipMemberController {
+    private final MembershipService membershipService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<MembershipResponseDto>>> findAllMemberships(
+            @ModelAttribute Pagecond pagecond
+    ) {
+        Page<MembershipResponseDto> responses = membershipService.findAllMemberships(pagecond);
+        PageInfo pageInfo = PageInfo.builder()
+                .pageNum(pagecond.getPageNum())
+                .pageSize(pagecond.getPageSize())
+                .totalElement(responses.getTotalElements())
+                .totalPage(responses.getTotalPages())
+                .build();
+
+        return ResponseEntity.ok().body(ApiResponse.of(responses.getContent(), pageInfo));
+    }
+
+    @GetMapping("/{membershipId}")
+    public ResponseEntity<ApiResponse<MembershipResponseDto>> findMembershipById(
+            @PathVariable("membershipId") Long membershipId
+    ) {
+        return ResponseEntity.ok().body(ApiResponse.of(membershipService.findMembershipById(membershipId)));
+    }
+}
