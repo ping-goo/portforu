5e80bcbc596f409ff494c19f22b00213408fb2ac
ChungWon <wjdcjddnjs1234@gmail.com>
2025-04-14 22:31:03 +0900

    ref : security add public

--- src/main/java/org/pinggu/portforu/config/SecurityConfig.java
+++ src/main/java/org/pinggu/portforu/config/SecurityConfig.java
@@ -37,8 +37,8 @@ public class SecurityConfig {
                 .logout(AbstractHttpConfigurer::disable)
                 .rememberMe(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**", "/oauth2/**", "/login/**", "/error", "/favicon.ico","/pay",              // ✅ 결제페이지"/pay/**","/templates/**",
-                                "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/public/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo
