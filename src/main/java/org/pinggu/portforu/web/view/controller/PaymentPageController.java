3f5612e63f8b22d9a11a5f290bbd475f15c83169
ansdudn2 <ansdudn2@naver.com>
2025-04-21 10:55:51 +0900

    fix:expired can't go paymentwindow

--- src/main/java/org/pinggu/portforu/web/view/controller/PaymentPageController.java
+++ src/main/java/org/pinggu/portforu/web/view/controller/PaymentPageController.java
@@ -1,11 +1,16 @@
 package org.pinggu.portforu.web.view.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.pinggu.portforu.common.exception.CustomException;
 import org.pinggu.portforu.domain.member.entity.Member;
+import org.pinggu.portforu.domain.payment.entity.Payment;
+import org.pinggu.portforu.domain.payment.enums.PaymentStatus;
+import org.pinggu.portforu.domain.payment.service.PaymentFinder;
 import org.pinggu.portforu.domain.subscribe.entity.Subscribe;
 import org.pinggu.portforu.domain.subscribe.service.SubscribeFinder;
 import org.pinggu.portforu.domain.subscribe.service.SubscribeService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PaymentPageController {
 
     private final SubscribeFinder subscribeFinder;
+    private final PaymentFinder paymentFinder;
 
     @Value("${toss.test-client-key}")
     private String clientKey;
@@ -33,6 +39,11 @@ public class PaymentPageController {
 
         try {
             Subscribe subscribe = subscribeFinder.findById(subscribeId);
+            Payment payment = paymentFinder.findBySubscribeId(subscribeId);
+
+            if (payment.getStatus() != PaymentStatus.PENDING) {
+                throw new CustomException(HttpStatus.BAD_REQUEST, "만료되었거나 유효하지 않은 결제입니다.");
+            }
             Member member = subscribe.getMember();
 
             model.addAttribute("clientKey", clientKey);
