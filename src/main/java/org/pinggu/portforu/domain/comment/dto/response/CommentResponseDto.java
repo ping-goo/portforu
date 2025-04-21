955f983cddae8da49b8fa50178c80d6ddd9441f8
Kim Hani <rlahani99@gmail.com>
2025-04-21 11:46:41 +0900

    fix/commetDto : Add email in CommentResponseDto

--- src/main/java/org/pinggu/portforu/domain/comment/dto/response/CommentResponseDto.java
+++ src/main/java/org/pinggu/portforu/domain/comment/dto/response/CommentResponseDto.java
@@ -14,6 +14,7 @@ public class CommentResponseDto {
     private final Long memberId;
     private final Long portfolioId;
     private final String content;
+    private final String email;
     private final Instant createdAt;
     private final Instant updatedAt;
     private final Instant deletedAt;
@@ -24,6 +25,7 @@ public class CommentResponseDto {
                 .memberId(comment.getMember().getId())
                 .portfolioId(comment.getPortfolio().getId())
                 .content(comment.getContent())
+                .email(comment.getMember().getEmail())
                 .createdAt(comment.getCreatedAt())
                 .updatedAt(comment.getUpdatedAt())
                 .deletedAt(comment.getDeletedAt())
