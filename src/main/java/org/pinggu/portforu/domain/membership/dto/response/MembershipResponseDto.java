8fc44966f7653f735f9e4023076bd8ec942f4d3c
ChungWon <wjdcjddnjs1234@gmail.com>
2025-04-22 16:44:17 +0900

    ref : resonseDto delete updateAt

--- src/main/java/org/pinggu/portforu/domain/membership/dto/response/MembershipResponseDto.java
+++ src/main/java/org/pinggu/portforu/domain/membership/dto/response/MembershipResponseDto.java
@@ -15,7 +15,6 @@ public class MembershipResponseDto {
     private final Integer quantity;
     private final Integer year;
     private final Instant createdAt;
-    private final Instant updatedAt;
     private final Boolean isDeleted;
 
     public static MembershipResponseDto from(Membership membership) {
@@ -26,7 +25,6 @@ public class MembershipResponseDto {
                 .quantity(membership.getQuantity())
                 .year(membership.getYear())
                 .createdAt(membership.getCreatedAt())
-                .updatedAt(membership.getUpdatedAt())
                 .isDeleted(membership.getIsDeleted())
                 .build();
     }
