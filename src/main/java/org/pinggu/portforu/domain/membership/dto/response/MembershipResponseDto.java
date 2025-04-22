695790867324ed2b47f780bf48deaace29ae42b7
ChungWon <wjdcjddnjs1234@gmail.com>
2025-04-22 16:46:25 +0900

    ref : resonseDto reAdd updateAt

--- src/main/java/org/pinggu/portforu/domain/membership/dto/response/MembershipResponseDto.java
+++ src/main/java/org/pinggu/portforu/domain/membership/dto/response/MembershipResponseDto.java
@@ -15,6 +15,7 @@ public class MembershipResponseDto {
     private final Integer quantity;
     private final Integer year;
     private final Instant createdAt;
+    private final Instant updatedAt;
     private final Boolean isDeleted;
 
     public static MembershipResponseDto from(Membership membership) {
@@ -25,6 +26,7 @@ public class MembershipResponseDto {
                 .quantity(membership.getQuantity())
                 .year(membership.getYear())
                 .createdAt(membership.getCreatedAt())
+                .updatedAt(membership.getUpdatedAt())
                 .isDeleted(membership.getIsDeleted())
                 .build();
     }
