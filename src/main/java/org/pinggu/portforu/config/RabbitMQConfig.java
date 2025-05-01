f4232928f5196b19ae03ea37001b1dcef9037a1c
J kim <kimjiyeong1000@gmail.com>
2025-05-01 20:00:39 +0900

    fix: add bean for json

--- src/main/java/org/pinggu/portforu/config/RabbitMQConfig.java
+++ src/main/java/org/pinggu/portforu/config/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package org.pinggu.portforu.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +14,22 @@ public class RabbitMQConfig {
     @Bean
     public Queue commentCreatedQueue() {
         return new Queue("comment-created-queue", true);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // 🧩 컨테이너에 컨버터 주입
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter
+    ) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        return factory;
     }
 }
