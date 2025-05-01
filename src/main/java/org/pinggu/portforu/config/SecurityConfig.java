679951eed07dadf35b3b77cbd1625a17bf144257
Kim Hani <rlahani99@gmail.com>
2025-05-01 22:57:55 +0900

    feat/swagger : Swagger Integration

--- src/main/java/org/pinggu/portforu/config/SecurityConfig.java
+++ src/main/java/org/pinggu/portforu/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
-                                "/swagger-ui/**"
+                                "/swagger-ui/**",
+                                "/swagger-ui/index.html"
                         ).permitAll()
                         .requestMatchers(
                                 "/api/v1/auth/**",
